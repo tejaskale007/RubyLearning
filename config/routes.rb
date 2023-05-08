68e330d34d678bddbfc614e3de03441fec4e31c0
tejas-coditas <tejas.kale@coditas.com>
2023-05-08 16:23:55 +0530

    commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   
-  get "about", to: "about#index", as: :about
+  get "about-us", to: "about#index", as: :about # 'about' will reflect in rails routes, no matter what we have written in mapping
 
   root to: "main#index"
 
