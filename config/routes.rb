50002588820811e38a448104e0ca6d7e86da2279
tejas-coditas <tejas.kale@coditas.com>
2023-05-08 15:18:50 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  
+  get "about", to: "about#index", as: :about
+
+  root to: "main#index"
+
+end
